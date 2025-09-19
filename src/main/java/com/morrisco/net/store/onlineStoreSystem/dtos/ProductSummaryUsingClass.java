3056abe3e9a6280e73d5156876b0bb1609ecb59d
Morris <morrischerryg@gmail.com>
2025-09-19 13:31:28 +0300

    Fetching Partial Data with Projections Using Class

---
+++ src/main/java/com/morrisco/net/store/onlineStoreSystem/dtos/ProductSummaryUsingClass.java
@@ -0,0 +1,15 @@
+package com.morrisco.net.store.onlineStoreSystem.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+
+@AllArgsConstructor
+@Getter
+@ToString
+public class ProductSummaryUsingClass {
+    private long Id;
+    private String Name;
+}
--- src/main/java/com/morrisco/net/store/onlineStoreSystem/repository/ProductRepository.java
+++ src/main/java/com/morrisco/net/store/onlineStoreSystem/repository/ProductRepository.java
@@ -1,6 +1,7 @@
 package com.morrisco.net.store.onlineStoreSystem.repository;
 
 import com.morrisco.net.store.onlineStoreSystem.dtos.ProductSummary;
+import com.morrisco.net.store.onlineStoreSystem.dtos.ProductSummaryUsingClass;
 import com.morrisco.net.store.onlineStoreSystem.entities.Category;
 import com.morrisco.net.store.onlineStoreSystem.entities.Product;
 import org.springframework.data.jpa.repository.Modifying;
@@ -60,5 +61,6 @@ public interface ProductRepository extends CrudRepository<Product, Long> {
     @Query(value = "update products set price = :newPrice where category_id = :categoryId ",nativeQuery = true)
     void updatePriceByCategory(@Param("newPrice") BigDecimal newPrice, @Param("categoryId") Byte categoryId);
 
-    List<ProductSummary> findByCategory(Category category);
+    //@Query("select p.id,p.name from Product p where p.category = :category")
+    List<ProductSummaryUsingClass> findByCategory(@Param("category") Category category);
 }
