a25f17baba44ca68abbee5ac8ddff022c729264e
Morris <morrischerryg@gmail.com>
2025-09-23 14:53:07 +0300

    Customizing Response Data

--- src/main/java/com/morrisco/net/store/onlineStoreSystem/dtos/UserDto.java
+++ src/main/java/com/morrisco/net/store/onlineStoreSystem/dtos/UserDto.java
@@ -1,14 +1,34 @@
 package com.morrisco.net.store.onlineStoreSystem.dtos;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Column;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 @AllArgsConstructor
 @Getter
 public class UserDto {
+    /**
+     * @JsonIgnore for excluding a field
+     * @JsonProperty for renaming a field
+     * @JsonInclude for excluding null or empty fields
+     * @JsonFormatt for formatting values especially date or numbers
+     */
+    @JsonIgnore
     private  Integer id;
+    @JsonProperty("user_name")
     private String name;
     private String email;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private String username;
+
+    @JsonFormat(pattern = "YYYY-MM-DD HH:MM:SS")
+    private LocalDateTime createdAt;
 
 }
--- src/main/java/com/morrisco/net/store/onlineStoreSystem/mappers/UserMapper.java
+++ src/main/java/com/morrisco/net/store/onlineStoreSystem/mappers/UserMapper.java
@@ -3,10 +3,13 @@ package com.morrisco.net.store.onlineStoreSystem.mappers;
 import com.morrisco.net.store.onlineStoreSystem.dtos.UserDto;
 import com.morrisco.net.store.onlineStoreSystem.entities.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.springframework.stereotype.Component;
 
 @Mapper(componentModel = "spring")
 
 public interface UserMapper {
+
+    @Mapping(target = "createdAt", expression = "java(java.time.LocalDateTime.now())")
     UserDto toDto (User user);
 }
