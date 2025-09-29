e9aec845be02fbbe4b93242756015a257e2eaa69
Morris <morrischerryg@gmail.com>
2025-09-29 17:01:31 +0300

    Authentication Method 1.Session Based-> a server creates  a session a small piec of memoery in server about the user and sends it over the network inform of a cookie a client stores it in browser so every time during communications a client send a request plus this cookie problem is not scalable for many Users 2.Token Base authentication a server issues a token to a client and clients uses that token for every communictation with servers benefits ->      1.stateless require less memory in server      2.scalable for many users      3. works well for front end apps  whats contain in a token 1.Header -> algo used to generate token and type of Token i.e Jwt 2.PayLoad ->public information about the user plus issued time 3.Secret key -> this is the key that always remain in the server and its used to check the authentic of token return back by the user (decoding)

---
+++ src/main/java/com/morrisco/net/store/onlineStoreSystem/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.morrisco.net.store.onlineStoreSystem.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    //security Filter chain defines how HTTP Request are Secured
+    @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        /**
+         * Three Steps To Follow
+         * 1.Stateless Session -> Token Based Auth
+         * 2.Disable CSRF (Cross Site Request Forgery) attack by tricking browser to send the request without client knowing it
+         * 3.Authorize Http Request
+         */
+        http.sessionManagement(c->
+                c.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.authorizeHttpRequests(c->c
+                //.requestMatchers("/carts/**").permitAll()
+                .anyRequest().authenticated());
+
+        return http.build();
+  }
+}
