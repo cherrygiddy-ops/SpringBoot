4d20da7637bfcd1fa3fa886ea8ba4189a3bf28c1
Morris <morrischerryg@gmail.com>
2025-09-30 12:56:02 +0300

    Managing Secrets

--- src/main/java/com/morrisco/net/store/onlineStoreSystem/services/JwtService.java
+++ src/main/java/com/morrisco/net/store/onlineStoreSystem/services/JwtService.java
@@ -3,20 +3,25 @@ package com.morrisco.net.store.onlineStoreSystem.services;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
-
 @Service
-@AllArgsConstructor
 public class JwtService {
+    @Value("${spring.jwt.secret}")
+    private String secret;
+
     public String generateTokens(String email){
         final long expirationDate =86400; //seconds in a day
         return Jwts.builder()
                 .subject(email)
                 .issuedAt(new Date())
                 .expiration(new Date(System.currentTimeMillis() +1000 *expirationDate) )
-                .signWith(Keys.hmacShaKeyFor("hello".repeat(8).getBytes()))
+                .signWith(Keys.hmacShaKeyFor(secret.getBytes()))
                 .compact();
+
     }
+
 }
