29848c9775964bbc40813ef495f4f14b39f4f14c
Morris <morrischerryg@gmail.com>
2025-09-30 16:54:58 +0300

    ACCESSING THE CURRENT USER

--- src/main/java/com/morrisco/net/store/onlineStoreSystem/controllers/AuthController.java
+++ src/main/java/com/morrisco/net/store/onlineStoreSystem/controllers/AuthController.java
@@ -2,6 +2,9 @@ package com.morrisco.net.store.onlineStoreSystem.controllers;
 
 import com.morrisco.net.store.onlineStoreSystem.dtos.JwtResponse;
 import com.morrisco.net.store.onlineStoreSystem.dtos.LoginRequest;
+import com.morrisco.net.store.onlineStoreSystem.dtos.UserDto;
+import com.morrisco.net.store.onlineStoreSystem.mappers.UserMapper;
+import com.morrisco.net.store.onlineStoreSystem.repositories.UserRepository;
 import com.morrisco.net.store.onlineStoreSystem.services.JwtService;
 import com.morrisco.net.store.onlineStoreSystem.services.UserService;
 import jakarta.validation.Valid;
@@ -11,6 +14,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -21,6 +26,8 @@ import java.util.Map;
 public class AuthController {
     private final AuthenticationManager authenticationManager;
     private final JwtService jwtService;
+    private final UserRepository userRepository;
+   private final UserMapper userMapper;
     @PostMapping("/login")
     public ResponseEntity<JwtResponse> login(@Valid @RequestBody LoginRequest loginRequest){
        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getEmail(),loginRequest.getPassword()));
@@ -34,10 +41,21 @@ public class AuthController {
     @PostMapping("/validate")
     public boolean  validateToken(@RequestHeader("Authorization") String authHeader){
 
-        System.out.println("validate called");
       var token = authHeader.replace("Bearer ","");
 
         return jwtService.validateToken(token);
+    }
+
+    @GetMapping("/currentUser")
+    public ResponseEntity<UserDto> currentUser (){
+    var authentication = SecurityContextHolder.getContext().getAuthentication();
+    var email =(String)authentication.getPrincipal();
+    var user = userRepository.findByEmail(email).orElse(null);
+     if (user==null)
+         return ResponseEntity.notFound().build();
+     var userDto = userMapper.toDto(user);
+
+     return ResponseEntity.ok(userDto);
     }
 
     @ExceptionHandler(BadCredentialsException.class)
